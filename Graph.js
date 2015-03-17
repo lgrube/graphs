b4431212030f416c3bba4f31ec20047916d31a1e
lgrube <lgv9483@saintjoe.edu>
2015-03-17 14:55:29 -0500

    modified Graph class with bfs

--- Graph.js
+++ Graph.js
@@ -2,6 +2,7 @@ function Graph(v) {
   this.vertices = v;
   this.edges = 0;
   this.adj = [];
+  this.edgeTo = [];
   for (var i = 0; i < this.vertices; ++i) {
     this.adj[i] = [];
     }
@@ -12,6 +13,9 @@ function Graph(v) {
   this.addEdge = addEdge;
   this.showGraph = showGraph;
   this.dfs = dfs;
+  this.pathTo = pathTo;
+  this.hasPathTo = hasPathTo;
+  this.showPath = showPath;
 }
 
 function addEdge(v,w) {
@@ -43,3 +47,52 @@ function dfs(v) {
       }
     }
 }
+
+function bfs(s) {
+	var queue =[];
+	this.marked[s] = true;
+	queue.push(s) //add to back of queue
+		while (queue.lenght > 0) {
+			var v = queue.shift(); //remove from front of queue
+			if (v !== undefined) {
+				print("Visited vertex: " + v);
+			}
+			for (var i=0; i<this.adj[v].lenght; i++) {
+				var w = this.adj[v][i];
+				if(!this.marked[w]) {
+					this.edgeTo[w] =v;
+					this.marked[w] = true;
+					queue.push(w);
+				}
+			}
+		}
+}
+
+function pathTo(source,v) {
+	if(!this.hasPathTo(v)) {
+		return undefined;
+	}
+	var path = [];
+	for(var i=v; i !=source; i = this.edgeTo[i]) {
+		path.push(i);
+	}
+	path.push(source);
+	return path;
+}
+
+function hasPathTo(v) {
+	return this.marked[v];
+}
+
+function showPath(paths) {
+	while (paths.length >0) {
+		if(paths.lenght >1) {
+			putstr(paths.pop() + '-');
+		}
+		else{
+			putstr(paths.pop());
+		}
+	}
+}
+
+
